17268c79965b40159c67c27bcc9cbb2ae7d6b4e3
Mika Isomaa <mika.isomaa@gmail.com>
2018-10-12 00:52:59 +0300

    some sort of popup system - dismisses in five seconds or with a click, and has a queue for many popups. adjusts to content size.

---
+++ Assets/PopupManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupManager : MonoBehaviour {
+
+    public struct popupInfo
+    {
+        public string title;
+        public string content;
+    };
+
+    public static PopupManager current;
+    public PopupScript popup;
+
+    public List<popupInfo> popupQueue;
+    bool queueRunning = false;
+
+    public void MakePopup(string title, string content)
+    {
+        popupInfo newPopupInfo = new popupInfo
+        {
+            title = title,
+            content = content
+        };
+        popupQueue.Add(newPopupInfo);
+        if (!queueRunning)
+        {
+            queueRunning = true;
+            StartCoroutine(startShowingPopups());
+        }
+    }
+
+    IEnumerator startShowingPopups()
+    {
+        while (popupQueue.Count > 0)
+        {
+            popup.NewPopup(popupQueue[0].title, popupQueue[0].content);
+            yield return null;
+            popupQueue.RemoveAt(0);
+            Debug.Log("Showing popup");
+            if (popupQueue.Count == 0)
+            {
+                Debug.Log("Stopping now");
+                yield return StartCoroutine(fadeSequence());
+                queueRunning = false;
+                popup.gameObject.SetActive(false);
+                break;
+            }
+            yield return StartCoroutine(fadeSequence());
+            popup.gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator fadeSequence()
+    {
+        float waitTime = 5f;
+        while (waitTime > 0f && popup.gameObject.activeSelf)
+        {
+            waitTime -= Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        popupQueue = new List<popupInfo>();
+        //MakePopup("testing", "123");
+        //MakePopup("testing", "456");
+        //MakePopup("testing", "789");
+    }
+	
+}
---
+++ Assets/PopupScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PopupScript : MonoBehaviour {
+    public TextMeshProUGUI UItitle;
+    public TextMeshProUGUI UIcontent;
+    
+    public void NewPopup(string title, string content)
+    {
+        gameObject.SetActive(false);
+        UItitle.text = title;
+        UIcontent.text = content;
+        gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
